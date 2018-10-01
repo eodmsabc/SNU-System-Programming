a2bfae2e38be081cc4ccb3133e0c2f308bf3b894
Minsu Kim <his4000@hanmail.net>
2018-10-01 11:25:55 +0900

    Kernel Lab skeleton code upload

---
+++ Lab/KernelLab/paddr/dbfs_paddr.c
@@ -0,0 +1,47 @@
+#include <linux/debugfs.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <asm/pgtable.h>
+
+MODULE_LICENSE("GPL");
+
+static struct dentry *dir, *output;
+static struct task_struct *task;
+
+static ssize_t read_output(struct file *fp,
+                        char __user *user_buffer,
+                        size_t length,
+                        loff_t *position)
+{
+        // Implement read file operation
+}
+
+static const struct file_operations dbfs_fops = {
+        // Mapping file operations with your functions
+};
+
+static int __init dbfs_module_init(void)
+{
+        // Implement init module
+
+        dir = debugfs_create_dir("paddr", NULL);
+
+        if (!dir) {
+                printk("Cannot create paddr dir\n");
+                return -1;
+        }
+
+        // Fill in the arguments below
+        output = debugfs_create_file("output", , , , );
+
+        return 0;
+}
+
+static void __exit dbfs_module_exit(void)
+{
+        // Implement exit module
+}
+
+module_init(dbfs_module_init);
+module_exit(dbfs_module_exit);
