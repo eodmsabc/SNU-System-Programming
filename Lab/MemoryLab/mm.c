be8d6dcb48775f53f3bc59fbc2a41ddda8b7267a
Richard <richard@csap.snu.ac.kr>
2018-10-15 15:36:45 +0900

    Memory lab's squeleton code upload

---
+++ Lab/MemoryLab/mm.c
@@ -0,0 +1,129 @@
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ *
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+/*********************************************************
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * provide your information in the following struct.
+ ********************************************************/
+team_t team = {
+  /* Team name : Your student ID */
+  "",
+  /* Your full name */
+  "",
+  /* Your student ID, again  */
+  "",
+  /* leave blank */
+  "",
+  /* leave blank */
+  ""
+};
+
+/* DON'T MODIFY THIS VALUE AND LEAVE IT AS IT WAS */
+static range_t **gl_ranges;
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* 
+ * remove_range - manipulate range lists
+ * DON'T MODIFY THIS FUNCTION AND LEAVE IT AS IT WAS
+ */
+static void remove_range(range_t **ranges, char *lo)
+{
+  range_t *p;
+  range_t **prevpp = ranges;
+  
+  if (!ranges)
+    return;
+
+  for (p = *ranges;  p != NULL; p = p->next) {
+    if (p->lo == lo) {
+      *prevpp = p->next;
+      free(p);
+      break;
+    }
+    prevpp = &(p->next);
+  }
+}
+
+/*
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(range_t **ranges)
+{
+  /* YOUR IMPLEMENTATION */
+
+
+  /* DON't MODIFY THIS STAGE AND LEAVE IT AS IT WAS */
+  gl_ranges = ranges;
+
+  return 0;
+}
+
+/*
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void* mm_malloc(size_t size)
+{
+  int newsize = ALIGN(size + SIZE_T_SIZE);
+  void *p = mem_sbrk(newsize);
+  if (p == (void *)-1)
+    return NULL;
+  else {
+    *(size_t *)p = size;
+    return (void *)((char *)p + SIZE_T_SIZE);
+  }
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr)
+{
+  /* YOUR IMPLEMENTATION */
+
+
+  /* DON't MODIFY THIS STAGE AND LEAVE IT AS IT WAS */
+  if (gl_ranges)
+    remove_range(gl_ranges, ptr);
+}
+
+/*
+ * mm_realloc - empty implementation; YOU DO NOT NEED TO IMPLEMENT THIS
+ */
+void* mm_realloc(void *ptr, size_t t)
+{
+  return NULL;
+}
+
+/*
+ * mm_exit - finalize the malloc package.
+ */
+void mm_exit(void)
+{
+}
+
