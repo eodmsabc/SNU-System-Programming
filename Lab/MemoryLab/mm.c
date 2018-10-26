1aab5ad64f9894c136621a39609dbc3b229ba226
Daeeun Kim <nebula5703@naver.com>
2018-10-26 22:25:52 +0900

    textbook

--- Lab/MemoryLab/mm.c
+++ Lab/MemoryLab/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
   /* Team name : Your student ID */
-  "",
+  "2014-15395",
   /* Your full name */
-  "",
+  "Daeeun Kim",
   /* Your student ID, again  */
-  "",
+  "2014-15395",
   /* leave blank */
   "",
   /* leave blank */
@@ -69,13 +69,62 @@ static void remove_range(range_t **ranges, char *lo)
   }
 }
 
+////////////////////////////// DEFINES ///////////////////////////////
+
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<<12)
+
+#define MIN_BLOCK_SIZE (2 * DSIZE)
+
+#define MAX(x, y) 	((x) > (y)? (x) : (y))
+#define MIN(x, y)	((x) > (y)? (y) : (x))
+
+#define PACK(size, alloc) 	((size) | (alloc))
+
+#define GET(p)			(*(unsigned int*)(p))
+#define PUT(p, val)		(*(unsigned int*)(p) = (val))
+
+#define GET_SIZE(p)		(GET(p) & ~0x7)
+#define GET_ALLOC(p)	(GET(p) & 0x1)
+
+#define HDRP(bp)	((char*)(bp) - WSIZE)
+#define FTRP(bp)	((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp)	((char*)(bp) + GET_SIZE((char*)(bp) - WSIZE))
+#define PREV_BLKP(bp)	((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))
+
+static void *prologue;
+
+static void *coalesce(void*);
+static void *find_fit(size_t);
+static void place(void*, size_t);
+
+static void *extend_heap(size_t size) {
+	char *bp;
+	if ((bp = mem_sbrk(size)) == (void*)-1) return NULL;
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+	
+	return coalesce(bp);
+}
+
 /*
  * mm_init - initialize the malloc package.
  */
 int mm_init(range_t **ranges)
 {
   /* YOUR IMPLEMENTATION */
+  if ((prologue = mem_sbrk(4*WSIZE)) == (void*)-1) return -1;
 
+  PUT(prologue, 0);
+  PUT(prologue + (1*WSIZE), PACK(DSIZE, 1));
+  PUT(prologue + (2*WSIZE), PACK(DSIZE, 1));
+  PUT(prologue + (3*WSIZE), PACK(0, 1));
+  prologue += (2*WSIZE);
+
+  if (extend_heap(CHUNKSIZE) == NULL) return -1;
 
   /* DON't MODIFY THIS STAGE AND LEAVE IT AS IT WAS */
   gl_ranges = ranges;
@@ -89,6 +138,7 @@ int mm_init(range_t **ranges)
  */
 void* mm_malloc(size_t size)
 {
+	/*
   int newsize = ALIGN(size + SIZE_T_SIZE);
   void *p = mem_sbrk(newsize);
   if (p == (void *)-1)
@@ -97,6 +147,21 @@ void* mm_malloc(size_t size)
     *(size_t *)p = size;
     return (void *)((char *)p + SIZE_T_SIZE);
   }
+  */
+  char *bp;
+  size_t extendsize;
+  if (size == 0) return NULL;
+  size = ALIGN(size + DSIZE);
+  if ((bp = find_fit(size)) != NULL) {
+	  place(bp, size);
+	  return bp;
+  }
+  extendsize = MAX(size, CHUNKSIZE);
+  if ((bp = extend_heap(extendsize)) == NULL) {
+	  return NULL;
+  }
+  place(bp, size);
+  return bp;
 }
 
 /*
@@ -105,7 +170,10 @@ void* mm_malloc(size_t size)
 void mm_free(void *ptr)
 {
   /* YOUR IMPLEMENTATION */
-
+  size_t size = GET_SIZE(HDRP(ptr));
+  PUT(HDRP(ptr), PACK(size, 0));
+  PUT(FTRP(ptr), PACK(size, 0));
+  coalesce(ptr);
 
   /* DON't MODIFY THIS STAGE AND LEAVE IT AS IT WAS */
   if (gl_ranges)
@@ -125,5 +193,67 @@ void* mm_realloc(void *ptr, size_t t)
  */
 void mm_exit(void)
 {
+	void *scanner = NEXT_BLKP(prologue);
+	size_t size;
+	while((size = GET_SIZE(HDRP(scanner)))) {
+		if (GET_ALLOC(HDRP(scanner))) mm_free(scanner);
+		scanner = NEXT_BLKP(scanner);
+	}
+//	mem_reset_brk();
 }
 
+static void *coalesce(void *bp) {
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if (prev_alloc && next_alloc) return bp;
+
+	else if (prev_alloc && !next_alloc) {
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+
+	else if (!prev_alloc && next_alloc) {
+		bp = PREV_BLKP(bp);
+		size += GET_SIZE(HDRP(bp));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+
+	else {
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		bp = PREV_BLKP(bp);
+		size += GET_SIZE(HDRP(bp));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+	return bp;
+}
+
+static void *find_fit(size_t size) {
+	void *bp = prologue;
+	while(GET_SIZE(HDRP(bp)) > 0) {
+		if (!GET_ALLOC(HDRP(bp)) && (size <= GET_SIZE(HDRP(bp)))) {
+			return bp;
+		}
+		bp = NEXT_BLKP(bp);
+	}
+	return NULL;
+}
+
+static void place(void *bp, size_t size) {
+	size_t bs = GET_SIZE(HDRP(bp));
+	if (bs - size >= MIN_BLOCK_SIZE) {
+		PUT(HDRP(bp), PACK(size, 1));
+		PUT(FTRP(bp), PACK(size, 1));
+		bp = NEXT_BLKP(bp);
+		PUT(HDRP(bp), PACK(bs-size, 0));
+		PUT(FTRP(bp), PACK(bs-size, 0));
+	}
+	else {
+		PUT(HDRP(bp), PACK(bs, 1));
+		PUT(FTRP(bp), PACK(bs, 1));
+	}
+}
