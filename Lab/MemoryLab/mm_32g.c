0d465967abf18afc64fe3c3c22dc11a28af52d03
Daeeun Kim <nebula5703@naver.com>
2018-11-04 03:11:28 +0900

    Change expr from group to bulk, use ifdef, update report

--- Lab/MemoryLab/mm_32g.c
+++ Lab/MemoryLab/mm_32g.c
@@ -6,7 +6,7 @@
  * All blocks have boundary tag. It will be used for coalescing & mm_exit.
  * find_fit function searches proper size class. If failed, it moves onto next size class list.
  * If new allocation request with small alligned size, It generates big block
- * which size is (GRP_CREATE_NUM) times as big as request. (I use 8 for this value)
+ * which size is (BULK_CREATE_NUM) times as big as request. (I use 8 for this value)
  * It was more effective to manage external fragmentation.
  * 
  */
@@ -70,12 +70,17 @@ static void remove_range(range_t **ranges, char *lo)
   }
 }
 
+
+// Variables
+static void *size_class_p;
+static void *prologue;
+
 ////////////////////////////// DEFINES ///////////////////////////////
 
 #define WSIZE 4
 #define DSIZE 8
 #define CHUNKSIZE (1<<9)
-#define GRP_CREATE_NUM (1<<3)
+#define BULK_CREATE_NUM (1<<3)
 
 #define MIN_BLOCK_SIZE (2 * DSIZE)
 
@@ -83,19 +88,18 @@ static void remove_range(range_t **ranges, char *lo)
 #define MIN(x, y)	((x) > (y)? (y) : (x))
 
 // 2nd LSB will be used as check bit.
-// It will be marked as 1 if that block was generetaed as free block group.
+// It will be marked as 1 if that block was generetaed as bulk block.
 //#define PACK(size, alloc) 	((size) | (alloc))
 #define FREETAG 0x0
 #define ALLOCTAG 0x1
-#define GROUPTAG 0x2
+#define BULKTAG 0x2
 #define PACK(size, tag) ((size) | (tag))
-//#define PACK_GRP(size, alloc) ((size) | (alloc) | 0x2)
 
 #define GET(p)			(*(unsigned int*)(p))
 #define PUT(p, val)		(*(unsigned int*)(p) = (val))
 
 #define GET_SIZE(p)		(GET(p) & ~0x7)
-#define GET_ISGRP(p)	(GET(p) & 0x2)
+#define GET_ISBULK(p)	(GET(p) & 0x2)
 #define GET_ALLOC(p)	(GET(p) & 0x1)
 
 #define HDRP(bp)		((char*)(bp) - WSIZE)
@@ -109,6 +113,42 @@ static void remove_range(range_t **ranges, char *lo)
 #define NEXT_BLKP(bp)	((char*)(bp) + GET_SIZE((char*)(bp) - WSIZE))
 #define PREV_BLKP(bp)	((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))
 
+// FOR mm_check function
+#define ERROR_ALLOC_IN_FREE_LIST	1
+#define ERROR_WRONG_SIZE_CLASS		2
+#define ERROR_COALESCE_FAILED		3
+#define ERROR_ISOLATED_FREE_BLOCK	4
+#define FREE_MARK(p)			(PUT(HDRP(p), (GET(HDRP(p)) | 0x4)))
+#define FREE_MARK_ERASE(p)		(PUT(HDRP(p), (GET(HDRP(p)) & ~0x4)))
+#define GET_ISMARKED(p)			(GET(HDRP(p)) & 0x4)
+
+//////////////////////////////// SIZE CLASS /////////////////////////
+//#define CLASS_NUM_16	// The number of size class become 16
+/////////////////////////////////////////////////////////////////////
+
+// 16 LEVEL SIZE CLASS
+#ifdef CLASS_NUM_16
+#define SIZE_CLASS_NUM	16
+
+#define SIZE_CLASS_1	0x10
+#define SIZE_CLASS_2	0x20
+#define SIZE_CLASS_3	0x30
+#define SIZE_CLASS_4	0x40
+#define SIZE_CLASS_5	0x60
+#define SIZE_CLASS_6	0x80
+#define SIZE_CLASS_7	0xc0
+#define SIZE_CLASS_8	0x100
+#define SIZE_CLASS_9	0x200
+#define SIZE_CLASS_10	0x400
+#define SIZE_CLASS_11	0x800
+#define SIZE_CLASS_12	0x1000
+#define SIZE_CLASS_13	0x2000
+#define SIZE_CLASS_14	0x4000
+#define SIZE_CLASS_15	0x8000
+#endif
+
+// 32 LEVEL SIZE CLASS
+#ifndef CLASS_NUM_16
 #define SIZE_CLASS_NUM	32
 
 #define SIZE_CLASS_1	0x10
@@ -126,8 +166,8 @@ static void remove_range(range_t **ranges, char *lo)
 #define SIZE_CLASS_13	0x140
 #define SIZE_CLASS_14	0x180
 #define SIZE_CLASS_15	0x1c0
-#define SIZE_CLASS_16	0x200
 
+#define SIZE_CLASS_16	0x200
 #define SIZE_CLASS_17	0x280
 #define SIZE_CLASS_18	0x300
 #define SIZE_CLASS_19	0x380
@@ -143,10 +183,10 @@ static void remove_range(range_t **ranges, char *lo)
 #define SIZE_CLASS_29	0x3000
 #define SIZE_CLASS_30	0x4000
 #define SIZE_CLASS_31	0x5000
+#endif
 
-static void *size_class_p;
-static void *prologue;
-
+// returns proper size class number (Integer value 0 ~ 31)
+// It works with get_class_root function.
 static int get_size_class(size_t asize) {
 	if (asize <= SIZE_CLASS_1) return 0;
 	else if (asize <= SIZE_CLASS_2) return 1;
@@ -164,6 +204,12 @@ static int get_size_class(size_t asize) {
 	else if (asize <= SIZE_CLASS_13) return 12;
 	else if (asize <= SIZE_CLASS_14) return 13;
 	else if (asize <= SIZE_CLASS_15) return 14;
+
+#ifdef CLASS_NUM_16
+	else return 15;
+#endif
+
+#ifndef CLASS_NUM_16
 	else if (asize <= SIZE_CLASS_16) return 15;
 
 	else if (asize <= SIZE_CLASS_17) return 16;
@@ -183,21 +229,70 @@ static int get_size_class(size_t asize) {
 	else if (asize <= SIZE_CLASS_30) return 29;
 	else if (asize <= SIZE_CLASS_31) return 30;
 	else return 31;
+#endif
 }
 
+// Returns root node of seg-list.
 static void *get_class_root(int class) {
 	return size_class_p + (DSIZE * class);
 }
 
+// Returns lower bound of size class
+static size_t get_lower_bound(int class) {
+	switch (class) {
+		case 0: return 0;	
+		case 1: return SIZE_CLASS_1;
+		case 2: return SIZE_CLASS_2;
+		case 3: return SIZE_CLASS_3;
+		case 4: return SIZE_CLASS_4;
+		case 5: return SIZE_CLASS_5;
+		case 6: return SIZE_CLASS_6;
+		case 7: return SIZE_CLASS_7;
+		case 8: return SIZE_CLASS_8;
+		case 9: return SIZE_CLASS_9;
+		case 10: return SIZE_CLASS_10;
+		case 11: return SIZE_CLASS_11;
+		case 12: return SIZE_CLASS_12;
+		case 13: return SIZE_CLASS_13;
+		case 14: return SIZE_CLASS_14;
+		case 15: return SIZE_CLASS_15;
+
+#ifdef CLASS_NUM_16
+		default: return 0x7fffffff;
+#endif
+
+#ifndef CLASS_NUM_16
+		case 16: return SIZE_CLASS_16;
+		case 17: return SIZE_CLASS_17;
+		case 18: return SIZE_CLASS_18;
+		case 19: return SIZE_CLASS_19;
+		case 20: return SIZE_CLASS_20;
+		case 21: return SIZE_CLASS_21;
+		case 22: return SIZE_CLASS_22;
+		case 23: return SIZE_CLASS_23;
+		case 24: return SIZE_CLASS_24;
+		case 25: return SIZE_CLASS_25;
+		case 26: return SIZE_CLASS_26;
+		case 27: return SIZE_CLASS_27;
+		case 28: return SIZE_CLASS_28;
+		case 29: return SIZE_CLASS_29;
+		case 30: return SIZE_CLASS_30;
+		case 31: return SIZE_CLASS_31;
+		default: return 0x7fffffff;
+#endif
+	}
+}
+
 // Function Declaration
+void mm_check(void);
 static void set_boundary_tag(void*, size_t, size_t);
 static void insert_block(void*, size_t);
 static void remove_from_list(void*);
 static void *extend_heap(size_t);
-static void *extend_heap_grp(size_t);
+static void *extend_heap_bulk(size_t);
 static void *coalesce(void*);
-static void *find_fit(size_t);
-static void place(void*, size_t);
+static void *find_fit(size_t, int*);
+static void place(void*, size_t, int);
 
 /*
  * mm_init - initialize the malloc package.
@@ -236,18 +331,19 @@ void* mm_malloc(size_t size)
 {
 	void *bp;
 	size_t asize = ALIGN(size + DSIZE);
+	int found_class;
 
 	if (size == 0) return NULL;
 
-	if ((bp = find_fit(asize)) != NULL) {
-		place(bp, asize);
+	if ((bp = find_fit(asize, &found_class)) != NULL) {
+		place(bp, asize, found_class);
 		return bp;
 	}
 
-	// Create new block as group (Only if the request size is small enough)
+	// Create new block as bulk (Only if the request size is small enough)
 	// CHUNKSIZE == 1 << 9 == 512
 	if (asize <= CHUNKSIZE) {
-		if ((bp = extend_heap_grp(asize)) == NULL) return NULL;
+		if ((bp = extend_heap_bulk(asize)) == NULL) return NULL;
 	}
 
 	// Normal case
@@ -255,19 +351,21 @@ void* mm_malloc(size_t size)
 		if ((bp = extend_heap(asize)) == NULL) return NULL;
 	}
 
-	place(bp, asize);
+	// No need to check size class becuase bp is newly created block.
+	place(bp, asize, 0);
+//	mm_check();
 	return bp;
 }
 
 /*
  * mm_free - Freeing a block
  * Set header & footer as free.
- * If it was assigned from free block group, It keeps group tag bit.
+ * If it was assigned from bulk block, It keeps bulk tag bit.
  */
 void mm_free(void *ptr)
 {
 	size_t size = GET_SIZE(HDRP(ptr));
-	set_boundary_tag(ptr, size, GET_ISGRP(HDRP(ptr)));
+	set_boundary_tag(ptr, size, GET_ISBULK(HDRP(ptr)));
 	insert_block(ptr, size);
 
 	coalesce(ptr);
@@ -278,11 +376,63 @@ void mm_free(void *ptr)
 }
 
 /*
- * mm_realloc - empty implementation; YOU DO NOT NEED TO IMPLEMENT THIS
+ * mm_realloc - Standalone Realloc
+ * ptr == NULL : same with malloc
+ * t == 0 : same with free
+ * if new size is smaller than orignal : shrink current block
+ * if new size is big && next is free : merge 2 blocks and then realloc again.
+ * && next is allocaed : allocate new memory space with mm_malloc. and copy data
  */
 void* mm_realloc(void *ptr, size_t t)
 {
-  return NULL;
+	void *newbp;
+	size_t copy_index = 0;
+	size_t orig_size;
+	size_t next_size;
+	size_t new_asize;
+
+	if (ptr == NULL) {
+		return mm_malloc(t);
+	}
+	if (t == 0) {
+		mm_free(ptr);
+		return NULL;
+	}
+
+	orig_size = GET_SIZE(HDRP(ptr));
+	new_asize = ALIGN(t + DSIZE);
+
+	// Shrink if newsize is smaller than orignal size.
+	if (orig_size - new_asize >= MIN_BLOCK_SIZE) {
+		set_boundary_tag(ptr, new_asize, GET_ISBULK(HDRP(ptr)) | ALLOCTAG);
+		set_boundary_tag(NEXT_BLKP(ptr), orig_size - new_asize, GET_ISBULK(HDRP(ptr)));
+		insert_block(NEXT_BLKP(ptr), orig_size - new_asize);
+		coalesce(NEXT_BLKP(ptr));
+		return ptr;
+	}
+	else if (new_asize <= orig_size) {
+		return ptr;
+	}
+
+	// Merge with next block if next is free
+	if (!GET_ALLOC(HDRP(NEXT_BLKP(ptr)))) {
+		next_size = GET_SIZE(HDRP(NEXT_BLKP(ptr)));
+		remove_from_list(NEXT_BLKP(ptr));
+		set_boundary_tag(ptr, orig_size + next_size, ALLOCTAG);
+		// After merge, start from the beginning by recursive call.
+		return mm_realloc(ptr, t);
+	}
+
+	// Allocate New Block
+	newbp = mm_malloc(t);
+	// Copy Data
+	while (copy_index < orig_size - DSIZE) {
+		PUT(newbp + copy_index, GET(ptr + copy_index));
+		copy_index += 4;
+	}
+	// Free original memory block
+	mm_free(ptr);
+	return newbp;
 }
 
 /*
@@ -292,11 +442,104 @@ void* mm_realloc(void *ptr, size_t t)
  */
 void mm_exit(void)
 {
-	void *scanner = NEXT_BLKP(prologue);
-	size_t size;
-	while((size = GET_SIZE(HDRP(scanner)))) {
-		if (GET_ALLOC(HDRP(scanner))) mm_free(scanner);
-		scanner = NEXT_BLKP(scanner);
+	void *probe = NEXT_BLKP(prologue);
+	while (GET_SIZE(HDRP(probe))) {
+		if (GET_ALLOC(HDRP(probe))) mm_free(probe);
+		probe = NEXT_BLKP(probe);
+	}
+}
+
+
+/*
+ * mm_check - Check if all blocks are correctly tagged and linked.
+ */
+void mm_check(void) {
+	int error_int1, error_int2;
+	void *error_address = NULL;
+
+	void *classroot;
+	void *probe, *hdrp;
+	size_t classlb, classub, size;
+	unsigned int error = 0;
+	int i;
+
+	// Check if all blocks are in proper size-class free list.
+	for (i = 0; i < SIZE_CLASS_NUM; i++) {
+		classroot = get_class_root(i);
+		probe = NEXT_FREE(classroot);
+		classlb = get_lower_bound(i);
+		classub = get_lower_bound(i+1);
+		while (probe != classroot) {
+			hdrp = HDRP(probe);
+			// Check if allocated block is connected
+			if (GET_ALLOC(hdrp)) {
+				error = ERROR_ALLOC_IN_FREE_LIST;
+				error_int1 = i;
+				error_address = probe;
+				break;
+			}
+			size = GET_SIZE(hdrp);
+			// Check if block is in proper size-class free list.
+			if (size <= classlb || ((!GET_ISBULK(hdrp)) && (classub < size))) {
+				error = ERROR_WRONG_SIZE_CLASS;
+				error_int1 = i;
+				error_int2 = size;
+				error_address = probe;
+				break;
+			}
+			// Mark all free blocks which are accessible from root node of each seglist.
+			FREE_MARK(probe);
+			probe = NEXT_FREE(probe);
+		}
+		if (error) break;
+	}
+
+	// Check if 2 free blocks are adjacent
+	// Check if unmarked free block exists.
+	if (!error) {
+		probe = NEXT_BLKP(prologue);
+		// until epilogue
+		while (GET_SIZE(HDRP(probe))) {
+			if (!GET_ALLOC(HDRP(PREV_BLKP(probe))) && !GET_ALLOC(HDRP(probe))) {
+				if (!(GET_ISBULK(HDRP(PREV_BLKP(probe))) && GET_ISBULK(HDRP(probe)))) {
+					error = ERROR_COALESCE_FAILED;
+					error_address = probe;
+					break;
+				}
+			}
+			// check if unmarked free block exists
+			if (!GET_ALLOC(HDRP(probe))) {
+				if (GET_ISMARKED(probe)) {
+					FREE_MARK_ERASE(probe);
+				}
+				else {
+					error = ERROR_ISOLATED_FREE_BLOCK;
+					error_address = probe;
+					break;
+				}
+			}
+			probe = NEXT_BLKP(probe);
+		}
+	}
+
+	switch (error) {
+		case ERROR_ALLOC_IN_FREE_LIST:
+			printf("Allocated block is in free list.\n");
+			printf("Size Class: %d\nError Location: %p\n", error_int1, error_address);
+			break;
+		case ERROR_WRONG_SIZE_CLASS:
+			printf("Free block is not in valid size class.\n");
+			printf("Size Class: %d\nCurrent Size: %d\n", error_int1, error_int2);
+			printf("Error Location: %p\n", error_address);
+			break;
+		case ERROR_COALESCE_FAILED:
+			printf("Coalesce Error\n");
+			printf("Error Location: %p\n", error_address);
+			break;
+		case ERROR_ISOLATED_FREE_BLOCK:
+			printf("Not accessible free block exists\n");
+			printf("Error Location: %p\n", error_address);
+			break;
 	}
 }
 
@@ -307,6 +550,9 @@ static void set_boundary_tag(void *bp, size_t asize, size_t tagbit) {
 }
 
 // Insert block into proper size class
+// Size class is determined by parameter 'asize' only. It does not check bp's header.
+// It works with bulk block implementation.
+// Insert new block at the beginning of the list.
 static void insert_block(void *bp, size_t asize) {
 	void *sclassp = get_class_root(get_size_class(asize));
 	SET_PREV_F(bp, sclassp);
@@ -316,12 +562,15 @@ static void insert_block(void *bp, size_t asize) {
 }
 
 // Remove block bp from size class list.
+// After remove the block, re-connect the pointers.
 static void remove_from_list(void *bp) {
 	SET_NEXT_F(PREV_FREE(bp), NEXT_FREE(bp));
 	SET_PREV_F(NEXT_FREE(bp), PREV_FREE(bp));
 }
 
 // Extend heap
+// Extend heap with mem_sbrk function. Then, update epilogue block
+// Insert newly extended block and coalesce.
 static void *extend_heap(size_t asize) {
 	char *bp;
 
@@ -335,24 +584,39 @@ static void *extend_heap(size_t asize) {
 	return coalesce(bp);
 }
 
-// Extend heap as group.
-static void *extend_heap_grp(size_t asize) {
+// Extend heap as bulk free block.
+// Basically same structure with extend_heap function.
+// But newly created block will be inserted into smaller class list.
+// This function not calls coalesce. It uses it's own coalescing mechanism.
+static void *extend_heap_bulk(size_t asize) {
 	char *bp;
-	size_t grpsize = GRP_CREATE_NUM * asize;
+	size_t bulksize = BULK_CREATE_NUM * asize;
 
-	// New block is created with grpsize big
-	if ((bp = mem_sbrk(grpsize)) == (void*)-1) return NULL;
+	// New block is created with bulksize big
+	if ((bp = mem_sbrk(bulksize)) == (void*)-1) return NULL;
 
-	set_boundary_tag(bp, grpsize, GROUPTAG);
+	// This function uses it's own coalescing mechanism.
+	// When previous block is free but generated as bulk block,
+	// that's the only case that 2 free blocks become adjacent.
+	if (!GET_ALLOC(HDRP(PREV_BLKP(bp))) && !GET_ISBULK(HDRP(PREV_BLKP(bp)))) {
+		bp = PREV_BLKP(bp);
+		remove_from_list(bp);
+		bulksize += GET_SIZE(HDRP(bp));
+	}
+
+	set_boundary_tag(bp, bulksize, BULKTAG);
 	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, ALLOCTAG));
-	// I use asize NOT grpsize. This blocks will be connected smaller size-class list.
+
+	// insert_block uses asize NOT bulksize.
+	// This blocks will be inserted into smaller size-class list than it's real size.
 	insert_block(bp, asize);
 
+	// It returns bp without calling coalesce function.
 	return bp;
 }
 
 // Coalesce free blocks
-// This function does not check if it was generated as free block group or not
+// This function does not check if it was generated as bulk block or not
 // to prevent unnecessary external fragmentation.
 static void *coalesce(void *bp) {
 	void *nextbp;
@@ -385,15 +649,17 @@ static void *coalesce(void *bp) {
 
 // Find blocks from size class lists.
 // Implemented with Segregated Free Lists (Segregated fits)
-static void *find_fit(size_t asize) {
+// Choosen classnum will be sent through class variable
+static void *find_fit(size_t asize, int *class) {
 	int classnum = get_size_class(asize);
 	void *sclassp;
 	void *probe;
 	for (;classnum < SIZE_CLASS_NUM; classnum++) {
 		sclassp = get_class_root(classnum);
 		probe = NEXT_FREE(sclassp);
-		while(probe != sclassp) {
+		while (probe != sclassp) {
 			if (GET_SIZE(HDRP(probe)) >= asize) {
+				*class = classnum;
 				return probe;
 			}
 			probe = NEXT_FREE(probe);
@@ -403,15 +669,18 @@ static void *find_fit(size_t asize) {
 }
 
 // Place new blocks
-// If the block is big enough and it was generated as group,
+// If the block is big enough and it was generated as bulk block,
 // New free block which was created from split remains same size-class free list.
-static void place(void *bp, size_t asize) {
+static void place(void *bp, size_t asize, int class) {
 	size_t bsize = GET_SIZE(HDRP(bp));
 
 	if (bsize - asize >= MIN_BLOCK_SIZE) {
-		// If selected free block was generated as group
-		if (GET_ISGRP(HDRP(bp))) {
-			set_boundary_tag(bp, asize, GROUPTAG | ALLOCTAG);
+		// If selected free block was generated as bulk block
+		// Remaining space should be big enough to fit into same size-class list.
+		// If it is not handled, new small free block will never be allocated.
+		// Because it's in seg-list, it should be re-inserted.
+		if (GET_ISBULK(HDRP(bp)) && (bsize - asize > get_lower_bound(class))) {
+			set_boundary_tag(bp, asize, BULKTAG | ALLOCTAG);
 
 			// Reorganize free block list
 			// Block shrinks. Not re-inserted into free list.
@@ -421,8 +690,9 @@ static void place(void *bp, size_t asize) {
 			SET_PREV_F(NEXT_FREE(bp), NEXT_BLKP(bp));
 
 			bp = NEXT_BLKP(bp);
-			set_boundary_tag(bp, bsize - asize, GROUPTAG | FREETAG);
+			set_boundary_tag(bp, bsize - asize, BULKTAG | FREETAG);
 		}
+		// Same implementation with textbook from here.
 		else {
 			remove_from_list(bp);
 			set_boundary_tag(bp, asize, ALLOCTAG);
